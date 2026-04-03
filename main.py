b9a7b94714fa2be203be166bfd2aaa4e8d546079
Tim Cox <alamias@cox.net>
2026-04-02 17:11:18 -0700

    added moveable and drawwable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from logger import log_state
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
@@ -9,6 +10,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 
     while True:
         log_state()
@@ -16,8 +25,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-
+        dt = pygame.time.Clock().tick(60) / 1000.0
+        
 
 if __name__ == "__main__":
     main()
